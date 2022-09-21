f0d457abe837d4a03f68a1cdfdb9b11f020e789e
TuhakarainaB <Bruce.Tuhakaraina@toiohomai.ac.nz>
2022-09-21 14:06:29 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
